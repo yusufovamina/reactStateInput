6cad56708cc2c819048936c3bafbf8e81a3beb7e
yusufovamina <133372100+yusufovamina@users.noreply.github.com>
2024-06-26 11:43:04 +0400

    commited

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,10 +4,10 @@ import { UserForm } from './UserForm';
 const App = () => {
   return (
     <div>
-      <h1>User Form</h1>
+      <h1>user form</h1>
       <UserForm />
     </div>
   );
 };
 
-export default App;
+export default App
